72b51744833f085cf9b2199987a0c95b88881939
jakubszczukin <jakub.szczukin@gmail.com>
2021-10-28 11:38:29 +0200

    28.10.2021

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.example.musicplayer
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.karumi.dexter.PermissionToken
+
+import com.karumi.dexter.listener.PermissionDeniedResponse
+
+import com.karumi.dexter.listener.PermissionGrantedResponse
+
+import com.karumi.dexter.listener.single.PermissionListener
+
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionRequest
+
+class MainActivity : AppCompatActivity() {
+
+    private var mPlayer: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+    private val hlsUrl =
+        "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playerView = findViewById(R.id.playerView)
+
+    }
+
+    private fun initPlayer() {
+        mPlayer = SimpleExoPlayer.Builder(this).build()
+        // Bind the player to the view.
+        playerView.player = mPlayer
+        mPlayer!!.playWhenReady = true
+        mPlayer!!.seekTo(playbackPosition)
+        mPlayer!!.prepare(buildMediaSource(), false, false)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || mPlayer == null) {
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+
+    private fun releasePlayer() {
+        if (mPlayer == null) {
+            return
+        }
+        playWhenReady = mPlayer!!.playWhenReady
+        playbackPosition = mPlayer!!.currentPosition
+        currentWindow = mPlayer!!.currentWindowIndex
+        mPlayer!!.release()
+        mPlayer = null
+    }
+
+    private fun buildMediaSource(): MediaSource {
+        val userAgent =
+            Util.getUserAgent(playerView.context, playerView.context.getString(R.string.app_name))
+
+        val dataSourceFactory = DefaultHttpDataSourceFactory(userAgent)
+        val hlsMediaSource =
+            HlsMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(hlsUrl))
+
+        return hlsMediaSource
+    }
+
+    /*
+    private fun permissionHandler(){
+
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse) { /* ... */
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) { /* ... */
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ){
+                    /* ... */
+                }
+            }).check()
+    }*/
+
+}
+
